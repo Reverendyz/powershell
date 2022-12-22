2c55a5ea2267a90b2f5a50ae879dbfa404554b7e
Felipe Moreira dos Santos <fmsantos1997@hotmail.com>
2022-12-22 13:36:24 -0300

    Validation

--- ValidateXML.ps1
+++ ValidateXML.ps1
@@ -1,13 +1,33 @@
 function ValidateXML {
     param (
-        [string[]] $tagNames
+        [string] $XmlFile,
+        [string] $SchemaFile,
+        [scriptblock] $ValidationEventHandler = { Write-Error $args[1].Exception }
     )
-    
-    [XML]$xmlFile = Get-Content ./file.xml
+    Write-Host $XmlFile $SchemaFile
 
-    foreach ($node in ($xmlFile.SelectNodes("/Root/Elements/*"))){
-        Write-Host $node.Name
+    $schemaReader = New-Object System.Xml.XmlTextReader $SchemaFile
+    $schema = [System.Xml.Schema.XmlSchema]::Read($schemaReader, $ValidationEventHandler)
+
+    $ret = $true
+    try {
+    $xml = New-Object System.Xml.XmlDocument
+    $xml.Schemas.Add($schema) | Out-Null
+    $xml.Load($XmlFile)
+    $xml.Validate({
+            throw ([PsCustomObject] @{
+                SchemaFile = $SchemaFile
+                XmlFile = $XmlFile
+                Exception = $args[1].Exception
+            })
+        })
+    } catch {
+        Write-Error $_
+        $ret = $false
+    } 
+    finally {
+        $schemaReader.Close()
     }
 }
 
-ValidateXML
+ValidateXML $args[0] $args[1]
